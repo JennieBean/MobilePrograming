92842b11e97af817d2c00599692d898dce3520cb
JennieBean <113585108+JennieBean@users.noreply.github.com>
2023-05-07 22:06:55 +0700

    Add files via upload

---
+++ baris_kolom.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class BarisKolom extends StatelessWidget {
+  const BarisKolom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Baris & Kolom"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: const [
+          Text("Baris 1, Kolom 1" "Baris 1, Kolom 2" "Baris 1, kolom 3"),
+          Text("Baris 2, Kolom 1" "Baris 2, Kolom 2" "Baris 2, Kolom 3"),
+          Text("Baris 3, Kolom 1" "Baris 3, Kolom 2" "Baris 3, Kolom 3")
+        ],
+      ),
+    );
+  }
+}
